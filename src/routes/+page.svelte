a07a08a62364944d840841e4490741c88b2d0128
Michel Atucha <miche.atucha@gmail.com>
2024-05-22 14:15:45 -0500

    svelte + tailwindcss

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="text-2xl font-cursive">Projects</h1>
+<div class="columns-1 md:columns-2">
+	<div class="bg-slate-50 shadow">
+		<h2>FIRST</h2>
+	</div>
+	<div class="bg-slate-200">
+		<h2>SECOND</h2>
+	</div>
+</div>
+
+<style lang="postcss">
+</style>
