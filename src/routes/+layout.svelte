628fe79e410aab7e3b3253f34370de94b2b73807
Sp1ke <miche.atucha@gmail.com>
2024-05-22 18:21:40 -0500

    adding flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,146 @@
 <script>
 	import '../app.css';
+	import {
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavUl,
+		NavHamburger,
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper
+	} from 'flowbite-svelte';
+	import {
+		ChartPieSolid,
+		GridSolid,
+		MailBoxSolid,
+		UserSolid,
+		ShoppingBagSolid,
+		ArrowRightToBracketOutline,
+		EditOutline,
+		FireSolid,
+		BookSolid,
+		RestoreWindowOutline,
+		LifeSaverSolid
+	} from 'flowbite-svelte-icons';
+	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
 </script>
 
-<div class="container mx-auto">
-	<slot />
+<div class="container p-2 lg:p-6">
+	<Navbar class="fixed start-0 top-0 z-20 w-full border-b px-2 py-2.5 sm:px-4">
+		<NavBrand href="/">
+			<span class="self-center whitespace-nowrap text-xl font-semibold">Sp1ke</span>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl>
+			<DarkMode />
+		</NavUl>
+	</Navbar>
+
+	<Sidebar>
+		<SidebarWrapper>
+			<SidebarGroup>
+				<SidebarItem label="Dashboard">
+					<svelte:fragment slot="icon">
+						<ChartPieSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Kanban" {spanClass}>
+					<svelte:fragment slot="icon">
+						<GridSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+					<svelte:fragment slot="subtext">
+						<span
+							class="ms-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+						>
+							Pro
+						</span>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Inbox" {spanClass}>
+					<svelte:fragment slot="icon">
+						<MailBoxSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+					<svelte:fragment slot="subtext">
+						<span
+							class="ms-3 inline-flex h-3 w-3 items-center justify-center rounded-full bg-primary-200 p-3 text-sm font-medium text-primary-600 dark:bg-primary-900 dark:text-primary-200"
+						>
+							3
+						</span>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Users">
+					<svelte:fragment slot="icon">
+						<UserSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Products">
+					<svelte:fragment slot="icon">
+						<ShoppingBagSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Sign In">
+					<svelte:fragment slot="icon">
+						<ArrowRightToBracketOutline
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Sign Up">
+					<svelte:fragment slot="icon">
+						<EditOutline
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+			</SidebarGroup>
+			<SidebarGroup border>
+				<SidebarItem label="Upgrade to Pro">
+					<svelte:fragment slot="icon">
+						<FireSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Documentation">
+					<svelte:fragment slot="icon">
+						<BookSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Components">
+					<svelte:fragment slot="icon">
+						<RestoreWindowOutline
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Help">
+					<svelte:fragment slot="icon">
+						<LifeSaverSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+			</SidebarGroup>
+		</SidebarWrapper>
+	</Sidebar>
+
+	<div class="p-6">
+		<slot />
+	</div>
 </div>
+
+<style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,50 @@
-<h1 class="text-2xl font-cursive">Projects</h1>
-<div class="columns-1 md:columns-2">
-	<div class="bg-slate-50 shadow">
-		<h2>FIRST</h2>
-	</div>
-	<div class="bg-slate-200">
-		<h2>SECOND</h2>
-	</div>
-</div>
+<script>
+	import { BottomNav, BottomNavItem, Tooltip, Skeleton, ImagePlaceholder } from 'flowbite-svelte';
+	import {
+		HomeSolid,
+		WalletSolid,
+		AdjustmentsVerticalOutline,
+		UserCircleSolid,
+		PlusOutline
+	} from 'flowbite-svelte-icons';
+</script>
 
-<style lang="postcss">
-</style>
+<Skeleton class="py-4" />
+<ImagePlaceholder class="pb-20" />
+
+<BottomNav position="absolute" navType="application" classInner="grid-cols-5">
+	<BottomNavItem btnName="Home" appBtnPosition="left">
+		<HomeSolid
+			class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+		/>
+		<Tooltip arrow={false}>Home</Tooltip>
+	</BottomNavItem>
+	<BottomNavItem btnName="Wallet" appBtnPosition="middle">
+		<WalletSolid
+			class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+		/>
+		<Tooltip arrow={false}>Wallet</Tooltip>
+	</BottomNavItem>
+	<div class="flex items-center justify-center">
+		<BottomNavItem
+			btnName="Create new item"
+			appBtnPosition="middle"
+			btnClass="inline-flex items-center justify-center w-10 h-10 font-medium bg-primary-600 rounded-full hover:bg-primary-700 group focus:ring-4 focus:ring-primary-300 focus:outline-none dark:focus:ring-primary-800"
+		>
+			<PlusOutline class="text-white" />
+			<Tooltip arrow={false}>Create new item</Tooltip>
+		</BottomNavItem>
+	</div>
+	<BottomNavItem btnName="Settings" appBtnPosition="middle">
+		<AdjustmentsVerticalOutline
+			class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+		/>
+		<Tooltip arrow={false}>Settings</Tooltip>
+	</BottomNavItem>
+	<BottomNavItem btnName="Profile" appBtnPosition="right">
+		<UserCircleSolid
+			class="mb-1 h-6 w-6 text-gray-500 group-hover:text-primary-600 dark:text-gray-400 dark:group-hover:text-primary-500"
+		/>
+		<Tooltip arrow={false}>Profile</Tooltip>
+	</BottomNavItem>
+</BottomNav>
