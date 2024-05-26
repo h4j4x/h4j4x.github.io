ecb6fa81d43cf5f8381ff47d200f2054eb8b9bd3
Sp1ke <miche.atucha@gmail.com>
2024-05-25 23:35:21 -0500

    side menu refactor

--- src/lib/Menubar.svelte
+++ src/lib/Menubar.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
 	import {
 		ChartPieSolid,
@@ -13,106 +13,69 @@
 		RestoreWindowOutline,
 		LifeSaverSolid
 	} from 'flowbite-svelte-icons';
+	import type { ComponentType } from 'svelte';
 
 	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+	let iconClass =
+		'h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white';
+
+	interface MenuItemType {
+		label: string;
+		icon: ComponentType;
+		subtext?: string;
+		subtextProps?: Record<string, any>;
+	}
+	let items: Array<MenuItemType> = [
+		{
+			label: 'Dashboard',
+			icon: ChartPieSolid
+		},
+		{
+			label: 'Kanban',
+			icon: GridSolid,
+			subtext: 'Pro',
+			subtextProps: {
+				class:
+					'ms-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300'
+			}
+		},
+		{
+			label: 'Inbox',
+			icon: MailBoxSolid,
+			subtext: '3',
+			subtextProps: {
+				class:
+					'ms-3 inline-flex h-3 w-3 items-center justify-center rounded-full bg-primary-200 p-3 text-sm font-medium text-primary-600 dark:bg-primary-900 dark:text-primary-200'
+			}
+		},
+		{
+			label: 'Users',
+			icon: UserSolid
+		},
+		{
+			label: 'Products',
+			icon: ShoppingBagSolid
+		}
+	];
 </script>
 
 <Sidebar asideClass="w-54 hidden lg:flex">
 	<SidebarWrapper>
 		<SidebarGroup>
-			<SidebarItem label="Dashboard">
-				<svelte:fragment slot="icon">
-					<ChartPieSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Kanban" {spanClass}>
-				<svelte:fragment slot="icon">
-					<GridSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-				<svelte:fragment slot="subtext">
-					<span
-						class="ms-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
-					>
-						Pro
-					</span>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Inbox" {spanClass}>
-				<svelte:fragment slot="icon">
-					<MailBoxSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-				<svelte:fragment slot="subtext">
-					<span
-						class="ms-3 inline-flex h-3 w-3 items-center justify-center rounded-full bg-primary-200 p-3 text-sm font-medium text-primary-600 dark:bg-primary-900 dark:text-primary-200"
-					>
-						3
-					</span>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Users">
-				<svelte:fragment slot="icon">
-					<UserSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Products">
-				<svelte:fragment slot="icon">
-					<ShoppingBagSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Sign In">
-				<svelte:fragment slot="icon">
-					<ArrowRightToBracketOutline
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Sign Up">
-				<svelte:fragment slot="icon">
-					<EditOutline
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-		</SidebarGroup>
-		<SidebarGroup border>
-			<SidebarItem label="Upgrade to Pro">
-				<svelte:fragment slot="icon">
-					<FireSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Documentation">
-				<svelte:fragment slot="icon">
-					<BookSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Components">
-				<svelte:fragment slot="icon">
-					<RestoreWindowOutline
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
-			<SidebarItem label="Help">
-				<svelte:fragment slot="icon">
-					<LifeSaverSolid
-						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-					/>
-				</svelte:fragment>
-			</SidebarItem>
+			{#each items as item, i (i)}
+				<SidebarItem label={item.label} spanClass={item.icon ? spanClass : undefined}>
+					<svelte:fragment slot="icon">
+						<svelte:component this={item.icon} class={iconClass} />
+					</svelte:fragment>
+					<svelte:fragment slot="subtext">
+						{#if item.subtext}
+							<span {...item.subtextProps}>
+								{item.subtext}
+							</span>
+						{/if}
+					</svelte:fragment>
+				</SidebarItem>
+			{/each}
 		</SidebarGroup>
 	</SidebarWrapper>
 </Sidebar>
